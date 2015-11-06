25e6d85e8c8fe3aed7620a5ca054c26890a32603
Aaron Parecki <aaron@parecki.com>
2015-11-05 19:20:38 -0800

    mediawiki-style links

--- lib/parser.php
+++ lib/parser.php
@@ -10,7 +10,13 @@ class Parser {
       }
     }
 
+    // Mediawiki-style external links [http://example.com example]
+    $source = preg_replace('/(?<!\\\)\[(http[^\] ]+) ([^\]]+)\]/', '[$2]($1)', $source);
+
+    // Wiki page links with alternate display value [[page|pages]]
     $source = preg_replace('|\[\[([^\]\|]+)\|([^\]]+)\]\]|', '[$2](/$1)', $source);
+
+    // Wiki page links like [[page]]
     $source = preg_replace('|\[\[([^\]]+)\]\]|', '[$1](/$1)', $source);
 
     return $source;
